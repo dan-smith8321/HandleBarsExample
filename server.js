82f5b6231b8ece5534652c9dffc6bd3f41bbd24c
Daniel Smith <Daniel.smith-18@students.plymouth.ac.ukgit config --global user.name dan-smith8321>
2020-03-26 12:43:47 +0000

    Adding ContactForm& Linking to Database

--- server.js
+++ server.js
@@ -2,20 +2,57 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars');
 
+var mongoose = require('mongoose');
+
+var bodyParser = require('body-parser');
+
+var Contact = require('./models/Contact');
+
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars({
     layoutsDir: __dirname + '/views/layouts',
     extname: 'hbs'
 }))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false}));
+
 app.get('/', (req, res )=>{
-    res.render('index' , { layout: 'main'});
+    Contact.find({}).lean()
+    .exec((err, contacts) =>{
+        if(contacts.length){
+        res.render('index' , { layout: 'main', contacts: contacts, contactsExist: true });
+        } else {
+        res.render('index' , { layout: 'main', contacts: contacts, contactsExist: false }); //send this info to/
+    }})
 });
 
+app.post('/addContact', (req, res) =>{
+    const { name, email, number } = req.body;
+    var contact = new Contact({
+          name,
+          email,
+          number 
+    });
+    contact.save();
+    res.redirect('/');
+})
 
 app.get('/about', (req, res )=>{
-    res.render('about me', { layout: 'main'})
+    res.render('about', { layout: 'main'});
 });
+
+mongoose.connect('mongodb://localhost:27017/handlebars' , {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then (() => {
+    console.log('Connected to the DB')
+})
+.catch((err) => {
+    console.log('Not Connected to the DB : ' + err);
+});
+
 
 //listening for requests on port 3000
 app.listen(3000, ()=>{
